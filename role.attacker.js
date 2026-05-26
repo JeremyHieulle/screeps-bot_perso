3f003c07e7ef2e866425ecadf40e0cae00ca6863
Jérémy HIEULLE <jeremy.hieulle@orange.fr>
2026-05-26 17:48:25 +0200

    test attacker fix

--- role.attacker.js
+++ role.attacker.js
@@ -39,7 +39,7 @@ module.exports = {
         }
 
         const hostiles_structures = creep.room.find(FIND_HOSTILE_STRUCTURES).filter(c => !isAlly(c.owner.username));
-        if (hostiles.length > 0 ) {
+        if (hostiles_structures.length > 0 ) {
             if (creep.attack(hostiles_structures[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(hostiles_structures[0])
             };
